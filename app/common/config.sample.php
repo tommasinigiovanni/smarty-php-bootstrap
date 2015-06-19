d852e1e6aea4cac9789cf82aedab981502b9b1f9
Igor Moiseev <moiseev.igor@gmail.com>
2015-06-19 23:45:47 +0200

    smarty template works

---
+++ app/common/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Config
+ */
+return array(
+   // Database conection config
+  'db' => array(
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'user'      => 'user',
+    'password'  => 'password',
+    'port'      => '3206',
+    'dbname'    => 'testapp'
+    )
+  );
--- public/index.php
+++ public/index.php
@@ -2,4 +2,13 @@
 /*
  * Main index
  */
+require_once('../app/lib/smarty/libs/Smarty.class.php');
+
+$config = require_once('../app/common/config.php');
+
+$smarty = new Smarty();
+$smarty->setTemplateDir('../app/templates');
+$smarty->setCompileDir('../app/templates_c');
+
+$smarty->display('index.html');
 
